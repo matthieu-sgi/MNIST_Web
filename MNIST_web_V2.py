1a030b261b5e7e6be258e6de951c2595066a797b
sn00wden <matthieu2603@gmail.com>
2022-12-05 17:40:20 +0100

    Adding a new version of MNIST

--- MNIST_web_V2.py
+++ MNIST_web_V2.py
@@ -2,8 +2,12 @@ from __future__ import annotations
 
 from collections import OrderedDict
 from enum import Enum
+from onnxsim import simplify
+from onnx_tf.backend import prepare as tf_prepare
+from tensorflowjs.converters.tf_saved_model_conversion_v2 import convert_tf_saved_model
 from time import monotonic
-from torch.nn import (Dropout, Linear, Module, Sequential, SiLU) 
+from torch import Tensor
+from torch.nn import (Dropout, Linear, Module, ReLU, Sequential) 
 from torch.optim import (AdamW, Optimizer)
 from torch.optim.lr_scheduler import (OneCycleLR, _LRScheduler)
 from torch.utils.data import (DataLoader, random_split)
@@ -13,8 +17,12 @@ from torchvision.datasets import MNIST
 from torchvision.transforms import (Compose, Lambda, RandomRotation, ToTensor)
 from tqdm import tqdm
 
+import onnx
 import torch
+import torch.quantization as quant
 import torch.nn.functional as F
+import torch.nn.utils.prune as prune
+import torch.onnx as tonnx
 torch.backends.cuda.matmul.allow_tf32 = True
 torch.backends.cudnn.allow_tf32 = True
 torch.backends.cudnn.benchmark = True
@@ -29,16 +37,19 @@ class Split(Enum):
     TEST : str = "Test"
 
 
-def fit_step(model: Module, loader: DataLoader, optim: Optimizer, scheduler: _LRScheduler, scaler: GradScaler, split: Split, device : str) -> tuple[float, float]:
+def fit_step(model: Module, loader: DataLoader, optim: Optimizer, scheduler: _LRScheduler, scaler: GradScaler, split: Split, mixed: bool = True) -> tuple[float, float]:
+    try: device = next(model.parameters()).device
+    except: device = "cpu"
+
     train = split == Split.TRAIN
     total_loss, total_acc = 0.0, 0.0
     
     model.train(mode=train)
     with torch.inference_mode(mode=not train), tqdm(loader, desc=split.value) as pbar:
         for x, l in pbar:
-            x, l = x.to(device), l.cuda(device)
+            x, l = x.to(device=device), l.to(device=device)
 
-            with autocast():
+            with autocast(enabled=mixed):
                 logits = model(x)
                 loss = F.cross_entropy(logits, l)
                 acc = (logits.argmax(dim=1) == l).sum() / x.size(0)
@@ -57,7 +68,7 @@ def fit_step(model: Module, loader: DataLoader, optim: Optimizer, scheduler: _LR
     return total_loss, total_acc
 
 
-def fit(model: Module, epochs: int, loaders: dict[Split, DataLoader], device : str) -> None:
+def fit(model: Module, epochs: int, loaders: dict[Split, DataLoader]) -> None:
     summary(model, (28 * 28, ))
     
     optim = AdamW(model.parameters())
@@ -65,9 +76,9 @@ def fit(model: Module, epochs: int, loaders: dict[Split, DataLoader], device : s
     scaler = GradScaler()
 
     for _ in tqdm(range(epochs), desc="Epoch"):
-        train_loss, train_acc = fit_step(model, loaders[Split.TRAIN], optim, scheduler, scaler, Split.TRAIN,device=device)
-        valid_loss, valid_acc = fit_step(model, loaders[Split.VALID], None, None, scaler, Split.VALID,device=device)
-    test_loss, test_acc = fit_step(model, loaders[Split.TEST], None, None, scaler, Split.TEST,device=device)
+        train_loss, train_acc = fit_step(model, loaders[Split.TRAIN], optim, scheduler, scaler, Split.TRAIN)
+        valid_loss, valid_acc = fit_step(model, loaders[Split.VALID], None, None, None, Split.VALID)
+    test_loss, test_acc = fit_step(model, loaders[Split.TEST], None, None, None, Split.TEST)
     print("----------------------------------")
     print(f"[TRAIN] loss: {train_loss:.2e} acc: {train_acc*100:.2f}%")
     print(f"[VALID] loss: {valid_loss:.2e} acc: {valid_acc*100:.2f}%")
@@ -76,39 +87,78 @@ def fit(model: Module, epochs: int, loaders: dict[Split, DataLoader], device : s
 
 
 @torch.inference_mode()
-def benchmark_step(model: Module, input_size: tuple, n: int, batch_size: int, device: torch.device, dtype: torch.dtype) -> None:
+def benchmark_step(model: Module, input_size: tuple, n: int, batch_size: int, device: torch.device, dtype: torch.dtype) -> tuple[float, float, float]:
     model = model.to(device=device, dtype=dtype)
-    x = torch.rand(*input_size).to(device=device, dtype=dtype)
+    x = torch.rand(batch_size, *input_size).to(device=device, dtype=dtype)
     dts = []
-    for _ in tqdm(range(n), desc=f"Benchmark batch_size: {batch_size} device: {device} dtype: {dtype}"):
+    for _ in range(n):
         t_start = monotonic()
         model(x)
         dts.append(monotonic() - t_start)
-    min_dt, avg_dt, max_dt = min(dts), sum(dts) / len(dts), max(dts)
-    print("----------------------------------")
-    print(f"Parameters batch_size: {batch_size} device: {device} dtype: {dtype}")
-    print(f"min: {min_dt * 1_000:.2f} ms  avg: {avg_dt * 1_000:.2f} ms  max: {max_dt * 1_000:.2f} ms")
-    print("----------------------------------")
+    return min(dts), sum(dts) / len(dts), max(dts)
+    
     
 
 @torch.inference_mode()
-def benchmark(model: Module, input_size: tuple, n: int) -> None:
+def benchmark(model: Module, input_size: tuple, n: int, devices: list[str], dtypes: list[str]) -> None:
     model = model.eval()
-    for device in [torch.device("cpu"), torch.device("cuda:0")]:
-        for dtype in [torch.float32, torch.float16] if device == torch.device("cuda:0") else [torch.float32]:
-            for batch_size in [2 ** i for i in range(8)]:
-                benchmark_step(model, input_size, n, batch_size, device, dtype)
+    print("----------------------------------")
+    for device in devices:
+        for dtype in dtypes:
+            try:
+                for batch_size in [2 ** i for i in range(12)]:
+                    min_dt, avg_dt, max_dt = benchmark_step(model, input_size, n, batch_size, torch.device(device), dtype)
+                    print(f"batch_size: {str(batch_size):>4s} device: {device:<6s} dtype: {dtype} | min: {min_dt * 1_000:.2f} ms  avg: {avg_dt * 1_000:.2f} ms  max: {max_dt * 1_000:.2f} ms")
+                print("----------------------------------")
+            except: ...
+
+@torch.inference_mode()
+def sparsity(model: Module, min_value: float) -> float:
+    parameters = list(model.parameters())
+    s = sum((param.data.abs() <= min_value).sum().item() for param in parameters)
+    n = sum(param.data.flatten().size(0) for param in parameters)
+    print("----------------------------------")
+    print(f"Sparsity: {s / n * 100:.2f}%  {s}/{n}")
+    print("----------------------------------")
+    return s / n
 
 
+def prune_desc(model: Module, desc: list[tuple[Module, str]], amount: float) -> None:
+    prune.global_unstructured(desc, pruning_method=prune.L1Unstructured, amount=amount)
+    for m, n in desc: prune.remove(m, n)
+
+
+def quantize(model: Module, delete: list[Module], fuse: list[tuple[str, str]], loader: DataLoader) -> Module:
+    for m in delete: del m
+    q_model = Sequential(OrderedDict({"q": quant.QuantStub(), "m": model.float().eval().cpu(), "d": quant.DeQuantStub()}))
+    q_model.qconfig = quant.get_default_qconfig('fbgemm')
+    q_model = quant.prepare(quant.fuse_modules(q_model, fuse))
+    q_model.fit_step(loader, None, None, None, Split.TEST, mixed=False)
+    return quant.convert(q_model)
+
+
+def jit(model: Module, example: Tensor, device: torch.device, dtype: torch.dtype) -> torch.jit.ScriptModule:
+    return torch.jit.optimize_for_inference(torch.jit.trace(model.eval().to(device=device), example.to(device=device, dtype=dtype)))
+
+
+Module.fit_step = fit_step
 Module.fit = fit
 Module.benchmark = benchmark
+Module.sparsity = sparsity
+Module.prune = prune_desc
+Module.save = lambda m, path: torch.save(m.state_dict(), path)
+Module.quantize = quantize
+Module.jit = jit
 
 
 if __name__ == "__main__":
+    MNIST_TEST_LEN = len(MNIST("/tmp/mnist", download=True, train=False, transform=Compose([ToTensor(), Lambda(lambda x: x.reshape(-1))])))
+    test_valid_frac = 0.5
+
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     sets = {
         Split.TRAIN: MNIST("/tmp/mnist", download=True, train=True, transform=Compose([ToTensor(), RandomRotation(45), Lambda(lambda x: x.reshape(-1))])),
-        **{s: d for s, d in zip([Split.VALID, Split.TEST], random_split(MNIST("/tmp/mnist", download=True, train=False, transform=Compose([ToTensor(), Lambda(lambda x: x.reshape(-1))])), [0.5, 0.5]))},
+        **{s: d for s, d in zip([Split.VALID, Split.TEST], random_split(MNIST("/tmp/mnist", download=True, train=False, transform=Compose([ToTensor(), Lambda(lambda x: x.reshape(-1))])), lengths=[int(MNIST_TEST_LEN * test_valid_frac), int(MNIST_TEST_LEN * (1 - test_valid_frac))]))},
     }
 
     loaders = {
@@ -117,14 +167,71 @@ if __name__ == "__main__":
         Split.TEST : DataLoader(sets[Split.TEST],  batch_size=256, shuffle=False, pin_memory=True, drop_last=False, num_workers=22),
     }
 
+    h_dim = 256
     model = Sequential(OrderedDict({
-        "i": Sequential(OrderedDict({"l1": Linear(28 * 28, 256), "d1": Dropout(0.2), "a1": SiLU()})),
-        "h": Sequential(OrderedDict({"l2": Linear(    256, 256), "d2": Dropout(0.2), "a2": SiLU()})),
-        "o": Linear(256, 10),
-    })).to(device)
+        "i": Sequential(OrderedDict({"l": Linear(28 * 28, h_dim), "d": Dropout(0.2), "a": ReLU()})),
+        "h": Sequential(OrderedDict({"l": Linear(  h_dim, h_dim), "d": Dropout(0.2), "a": ReLU()})),
+        "o": Linear(h_dim, 10),
+    })).cuda()
     model.o.weight.data.normal_(0, 0.02)
     model.o.bias.data.zero_()
 
-    model.fit(5, loaders,device = device)
-    model.benchmark((28 * 28, ), 100)
-    torch.save(model.state_dict(), "mnist.pt")
+    # ==== TRAIN
+    model.fit(5, loaders)
+    model.benchmark((28 * 28, ), 100, ["cpu", "cuda:0"], [torch.float32, torch.half])
+    
+    # ==== SPARSITY EXPLORATION
+    # from copy import deepcopy
+    # import matplotlib.pyplot as plt
+    # amounts, accuracies = [0.2, 0.5, 0.8, 0.81, 0.82, 0.83, 0.84, 0.85, 0.86, 0.87, 0.88, 0.89, 0.9], []
+    # for amount in [0.2, 0.5, 0.8, 0.81, 0.82, 0.83, 0.84, 0.85, 0.86, 0.87, 0.88, 0.89, 0.9]:
+    #     prunned = deepcopy(model)
+    #     prunned.prune(((model.i.l, "weight"), (model.h.l, "weight"), (model.o, "weight")), amount)
+    #     accuracies.append(prunned.fit_step(loaders[Split.TEST], None, None, None, Split.TEST)[1])
+    #     prunned.sparsity(0.0)
+    # plt.figure()
+    # plt.title("Accuracy vs Amounts")
+    # plt.plot([amount * 100 for amount in amounts], [acc * 100 for acc in accuracies])
+    # plt.xlabel("Amount (%)")
+    # plt.ylabel("Accuracy (%)")
+    # plt.show()
+
+    # ==== SPARSITY PRUNNING
+    model.sparsity(0.0)
+    model.prune(((model.i.l, "weight"), (model.h.l, "weight"), (model.o, "weight")), 0.7)
+    model.fit_step(loaders[Split.TEST], None, None, None, Split.TEST)
+    model.sparsity(0.0)
+    model.save("mnist.pt")
+
+    # ==== UINT8 QUANTIZATION
+    q_model = model.quantize(delete=[model.i.d, model.h.d], fuse=[["m.i.l", "m.i.a"], ["m.h.l", "m.h.a"]], loader=loaders[Split.TEST])
+    q_model.benchmark((28 * 28, ), 100, ["cpu"], [None])
+    q_model.fit_step(loaders[Split.TEST], None, None, None, Split.TEST, mixed=False)
+
+    # ==== JIT + UINT8 QUANTIZATION
+    traced_q_model = q_model.jit(next(iter(loaders[Split.TEST]))[0], device="cpu", dtype=torch.float32)
+    traced_q_model.benchmark((28 * 28, ), 100, ["cpu"], [None])
+
+    # ==== ONNX EXPORT
+    tonnx.export(
+        model.eval().cpu(),
+        next(iter(loaders[Split.TEST]))[0],
+        "mnist.onnx",
+        export_params=True,
+        opset_version=10,
+        do_constant_folding=True,
+        input_names=["input"],
+        output_names=["output"],
+        dynamic_axes={"input": {0: "batch_size"}, "output": {0: "batch_size"}},
+    )
+
+    # ==== ONNX SANITY CHECK
+    o_model = onnx.load("mnist.onnx")
+    onnx.checker.check_model(o_model)
+
+    # ==== MODEL ONNX GRAPH SIMPLIFICATION
+    o_model, check = simplify(o_model)
+
+    # ==== TENSORFLOW JS EXPORT
+    tf_prepare(o_model).export_graph("mnist.pb")
+    convert_tf_saved_model("mnist.pb", "mnist.js")
